2a04a8f914f8aba1ec7e08850c0d999561e03194
successPhil <pbasti1990@gmail.com>
2024-05-02 04:07:41 -0400

    updated conf

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,11 @@ import App from './App.tsx'
 import './index.css'
 import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 
-const base_url = import.meta.env.VITE_BASE_URL + "/api"
+// const base_url = import.meta.env.VITE_BASE_URL + "/api"
 
 // const base_url = 'http://localhost:4000/graphql'
 const client = new ApolloClient({
-  uri: base_url,
+  uri: 'http://pokemonko.com/api',
   cache: new InMemoryCache()
 })
 
